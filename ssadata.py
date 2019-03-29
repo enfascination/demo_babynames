17f6b36c5b1c0b2b52e2b2067e7f5a63bc240b9e
Seth Frey <moctodliamg@gmail.com>
2019-03-28 22:09:53 -0700

    generalized data loading to include more years

--- ssadata.py
+++ ssadata.py
@@ -1,13 +1,23 @@
-NAMES_LIST = "yob2013.txt"
+def loadSSADataByGender( year, dgender ):
+    """
+    Takes a year and a gender and  returns a dictionary mapping all names within that gender to its number of occurrences.
+    year, an integer,  is everything 1990 to 2017 (and 1970)
+    gender can be "M" or "F"
+    this function assumes that the working directory contains the social security administration's annual public "yobYYYY.txt" files
+    """
+    NAMES_LIST = "yob" + str(year) + ".txt"
+    boys = {}
+    girls = {}
+    for line in open(NAMES_LIST, 'r').readlines():
+        name, gender, count = line.strip().split(",")
+        count = int(count)
 
-boys = {}
-girls = {}
-
-for line in open(NAMES_LIST, 'r').readlines():
-    name, gender, count = line.strip().split(",")
-    count = int(count)
-
-    if gender == "F":
-        girls[name.lower()] = count
-    elif gender == "M":
-        boys[name.lower()] = count
+        if gender == "F":
+            girls[name.lower()] = count
+        elif gender == "M":
+            boys[name.lower()] = count
+    if rGender == "M":
+        name_counts = boys
+    else:
+        name_counts = girls
+    return( name_counts )
