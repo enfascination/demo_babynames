5ed41ce6b178725bd0fb227b8066fd51fd4f8851
Seth Frey <moctodliamg@gmail.com>
2019-04-02 01:06:33 -0700

    generalized load function to be easier to use in colab, backwards compatible, and morre opaque, and compatible with zip files "

--- ssadata.py
+++ ssadata.py
@@ -1,23 +1,49 @@
-def loadSSADataByGender( year, dgender ):
+import gzip
+
+def loadSSADataByGender( path, dgender ):
     """
     Takes a year and a gender and  returns a dictionary mapping all names within that gender to its number of occurrences.
     year, an integer,  is everything 1990 to 2017 (and 1970)
     gender can be "M" or "F"
     this function assumes that the working directory contains the social security administration's annual public "yobYYYY.txt" files
     """
-    NAMES_LIST = "ssadata/" + "yob" + str(year) + ".txt"
+    NAMES_LIST = path
     boys = {}
     girls = {}
-    for line in open(NAMES_LIST, 'r').readlines():
-        name, gender, count = line.strip().split(",")
-        count = int(count)
+    with gzip.open(NAMES_LIST, 'rt') as f:
+        for line in f:
+            name, gender, count = line.strip().split(",")
+            count = int(count)
 
-        if gender == "F":
-            girls[name.lower()] = count
-        elif gender == "M":
-            boys[name.lower()] = count
+            if gender == "F":
+                girls[name.lower()] = count
+            elif gender == "M":
+                boys[name.lower()] = count
     if dgender == "M":
         name_counts = boys
     else:
         name_counts = girls
     return( name_counts )
+
+def loadSSADataIntoColab( year):
+    boys = loadSSADataByGender( "/content/demo_babynames/ssadata/" + "yob" + str(year) + ".txt.gz", "M")
+    girls = loadSSADataByGender( "/content/demo_babynames/ssadata/" + "yob" + str(year) + ".txt.gz", "F")
+    all = dict( boys )
+    for name, count in girls.items():
+        if name in all:
+            all[ name ] = all[ name ] + count
+        else:
+            all[ name ] = 0 + count
+    return( boys, girls, all )
+
+NAMES_LIST = "yob2013.txt"
+NAMES_PATH = "ssadata/" + NAMES_LIST+ ".gz"
+boys = loadSSADataByGender( NAMES_PATH, 'M' )
+girls = loadSSADataByGender( NAMES_PATH, 'F' )
+all = dict( boys )
+for name, count in girls.items():
+    if name in all:
+        all[ name ] = all[ name ] + count
+    else:
+        all[ name ] = 0 + count
+print( all )
